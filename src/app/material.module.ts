2c3f110ea2a3043b1990ad2d80353785301a5a9b
Prafful Jagtap <praffulamexs@bitbucket.org>
2019-02-14 02:41:23 +0530

    Add Material Design to the project

---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material';
+
+const components = [
+    MatButtonModule
+];
+
+@NgModule({
+    imports: [...components],
+    exports: [...components]
+})
+export class MaterialModule { }
